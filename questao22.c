99b1c7df1c94e7db42c88a6b0504394c7251d13e
Breno Pinheiro de Moura <143467828+BrenoIFCE@users.noreply.github.com>
2023-08-28 23:28:12 -0300

    questao22.c

---
+++ questao22.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+printf("A = %d\nB = %d\nC = %d\na = %d\nb = %d\nc = %d\n0 = %d\n1 = %d\n2 = %d\n$ = %d\n* = %d\n+ = %d\n/ = %d\nEspaço = %d\n", 'A', 'B', 'C', 'a', 'b', 'c', '0', '1', '2', '$', '*', '+', '/', ' ');
+  return 0;
+}
