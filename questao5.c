0930a61198bd66eb793342cfaa3d782f2771e07d
Breno Pinheiro de Moura <143467828+BrenoIFCE@users.noreply.github.com>
2023-08-28 23:23:31 -0300

    questao5.c

--- questao5.c
+++ questao5.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(void) {
+
+  float conta = 0;
+
+  puts("Digite o valor da conta do restaurante:");
+  printf("R$ ");
+  scanf("%f", &conta);
+
+  printf("O valor total a pagar será R$ %.2f", conta + conta * 0.1);
+
+  return 0;
+}
